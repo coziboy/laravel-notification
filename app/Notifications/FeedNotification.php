0a4249bc317be11adc2a5e8adf0b61f1cfa89680
Andreas Asatera Sandi Nofa <andreas.nofa@altern8.sg>
2024-04-17 21:45:44 +0700

    add telegram notification

--- app/Notifications/FeedNotification.php
+++ app/Notifications/FeedNotification.php
@@ -6,6 +6,8 @@ use App\Models\Feed;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use JsonException;
+use NotificationChannels\Telegram\TelegramMessage;
 
 class FeedNotification extends Notification
 {
@@ -32,6 +34,9 @@ class FeedNotification extends Notification
         if ($notifiable->email_verified_at) {
             $channels[] = 'mail';
         }
+        if ($notifiable->telegram_id) {
+            $channels[] = 'telegram';
+        }
 
         return $channels;
     }
@@ -39,7 +44,7 @@ class FeedNotification extends Notification
     /**
      * Get the mail representation of the notification.
      */
-    public function toMail(object $notifiable): MailMessage
+    public function toMail(): MailMessage
     {
         $feed = $this->feed;
         $user = $feed->user;
@@ -56,11 +61,28 @@ class FeedNotification extends Notification
      *
      * @return array<string, mixed>
      */
-    public function toArray(object $notifiable): array
+    public function toArray(): array
     {
         return [
             'title' => 'New Post Published',
             'message' => "{$this->feed->user->name} has published a new post.",
         ];
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function toTelegram($notifiable)
+    {
+        $url = route('feeds.index');
+
+        return TelegramMessage::create()
+            // Optional recipient user id.
+            ->to($notifiable->telegram_id)
+            // Markdown supported.
+            ->content('**New Post Published**'.PHP_EOL.PHP_EOL)
+            ->line("{$this->feed->user->name} has published a new post.".PHP_EOL)
+            ->line('Thank you for using our application!')
+            ->button('View Post', $url);
     }
 }
